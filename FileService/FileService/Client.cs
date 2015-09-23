9545f661a75007ce07ca8bfc3df7efae24a8a745
Claudio Sanchez <claudio@megsoftconsulting.com>
2015-09-22 21:50:44 -0400

    initial Console Self Host

---
+++ FileService/FileService/Client.cs
@@ -0,0 +1,6 @@
+﻿namespace FileService
+{
+    internal class Client
+    {
+    }
+}
---
+++ FileService/WebServiceHost.Console/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace WebServiceHost.Console
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            var config = new HttpConfiguration();
+            
+            config
+                .Routes
+                .MapHttpRoute("Api v1.0", "api/{controller}/");
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
